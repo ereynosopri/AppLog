6b9e164d2b6e5b3e45c446749a574c4f951a0760
ereynosopri <ereynosopri@gmail.com>
2026-04-29 16:50:54 -0700

    Delete lib directory

--- lib/server-supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export function createServerSupabase() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-  const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
-  return createClient(supabaseUrl, serviceRoleKey);
-}
